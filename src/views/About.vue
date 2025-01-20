9fb09380f919d3944dd090488c88479bdd96b322
kim-cheolwoon <kimcheolwoon@allips.kr>
2025-01-20 16:35:27 +0900

    [fixed] 이미지  빌드 오류 import 문으로 변경

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,17 +2,17 @@
 <main>
   <section class="relative">
     <img src="/src/assets/img/about_img.png" class="max-h-[400px] w-full object-cover" alt="">
-    <div class="items-center flex justify-center w-full h-full text-white absolute inset-0">
-        <h1 class="text-center text-5xl font-extrabold ">회사소개</h1>
+    <div class="absolute inset-0 flex items-center justify-center w-full h-full text-white">
+        <h1 class="text-5xl font-extrabold text-center ">회사소개</h1>
     </div>
   </section>
 
-  <section class="px-40  py-24 bg-gray-100">
-    <h1 class="text-4xl mb-8 font-extrabold ">회사 개요</h1>
+  <section class="px-40 py-24 bg-gray-100">
+    <h1 class="mb-8 text-4xl font-extrabold ">회사 개요</h1>
     <div class="flex justify-between">
       <div class="flex flex-col gap-12">
 
-          <p class="text-2xl text-blue-950 font-bold">로보틱박스는 자동화 기계와 데이터·AI 기술을 바탕으로 <br>
+          <p class="text-2xl font-bold text-blue-950">로보틱박스는 자동화 기계와 데이터·AI 기술을 바탕으로 <br>
               제조 및 로보틱스 분야에서 혁신적인 솔루션을 제공하는 전문 기업입니다.</p>
               <p class="mt-3">(주)로보틱박스는 2021년 설립된 연구소 기업으로,<br>경남 창원에 본사를 두고 있으며 한국전기연구원과의 합작법인으로 출범하였습니다.<br>고객의 안전과 효율성을 최우선으로 고려하며,산업 환경에서의 실시간 데이터 분석과<br>리스크 예측을 통해 생산성 향상 및 안전 강화를 위한 첨단 기술을 개발하고 있습니다.
               </p>
@@ -54,13 +54,13 @@
   </section>
 
 
-  <section class="px-40  py-24">
-    <h1 class="text-4xl mb-8 font-extrabold ">사업 영역</h1>
-    <p class="text-2xl text-blue-950 font-bold">로보틱박스는 로보틱스 및 AI 전문가들로 구성되어 첨단 기술 개발 및 고도화에 주력하고 있습니다.</p>
+  <section class="px-40 py-24">
+    <h1 class="mb-8 text-4xl font-extrabold ">사업 영역</h1>
+    <p class="text-2xl font-bold text-blue-950">로보틱박스는 로보틱스 및 AI 전문가들로 구성되어 첨단 기술 개발 및 고도화에 주력하고 있습니다.</p>
 
     <ul class="grid grid-cols-3 gap-2 mt-6">
         <li class="h-[400px] relative rounded-sm overflow-hidden border border-gray-50">
-            <div class="white-gra-col h-full absolute inset-0 flex flex-col justify-end px-4 py-8 gap-2">
+            <div class="absolute inset-0 flex flex-col justify-end h-full gap-2 px-4 py-8 white-gra-col">
                 <h1 class="text-xl font-bold">상용 PLC 및 로봇암 관리 엣지 디바이스</h1>
                 <p class="text-sm">로봇 및 공정 상태를 실시간으로 관리하는 엣지 디바이스를
                     개발하여 효율적인 공정 운영을 지원합니다.</p>
@@ -69,7 +69,7 @@
         </li>
 
         <li class="h-[400px] relative rounded-sm overflow-hidden border border-gray-50">
-            <div class="white-gra-col h-full absolute inset-0 flex flex-col justify-end px-4 py-8 gap-2">
+            <div class="absolute inset-0 flex flex-col justify-end h-full gap-2 px-4 py-8 white-gra-col">
                 <h1 class="text-xl font-bold">AI 기반 안전 관제 솔루션</h1>
                 <p class="text-sm">물류 사고를 줄이기 위한 지게차 안전 관제 시스템 등 IOT와 AI 기술을 융합한 안전 관리 솔루션을 제공합니다.</p>
             </div>
@@ -77,7 +77,7 @@
         </li>
 
         <li class="h-[400px] relative rounded-sm overflow-hidden border border-gray-50">
-            <div class="white-gra-col h-full absolute inset-0 flex flex-col justify-end px-4 py-8 gap-2">
+            <div class="absolute inset-0 flex flex-col justify-end h-full gap-2 px-4 py-8 white-gra-col">
                 <h1 class="text-xl font-bold">스마트 물류 이송 공정 구축</h1>
                 <p class="text-sm">웹 기반 상태인식 기능을 통해 공정 효율성을 극대화한 스마트 장치를 설계 및 구현합니다.</p>
             </div>
@@ -85,45 +85,45 @@
         </li>
     </ul>
     </section>
-    <section class="px-40  py-24 bg-gray-100">
-        <h1 class="text-4xl mb-8 font-extrabold ">핵심 가치</h1>
+    <section class="px-40 py-24 bg-gray-100">
+        <h1 class="mb-8 text-4xl font-extrabold ">핵심 가치</h1>
         <div class="flex items-center justify-center pt-1">
             <img src="@/assets/img/values.png" alt="" class="max-w-[1024px]">
         </div>
     </section>
 
-    <section class="px-40  py-24">
-        <h1 class="text-4xl mb-8 font-extrabold ">연혁</h1>
-        <ul class="flex flex-col relative gap-20 pl-20">
-            <li class="flex gap-32 relative">
-                <h1 class="text-blue-900 font-black text-3xl">2021'</h1>
-                <div class="flex flex-col gap-8 font-medium pt-1">
+    <section class="px-40 py-24">
+        <h1 class="mb-8 text-4xl font-extrabold ">연혁</h1>
+        <ul class="relative flex flex-col gap-20 pl-20">
+            <li class="relative flex gap-32">
+                <h1 class="text-3xl font-black text-blue-900">2021'</h1>
+                <div class="flex flex-col gap-8 pt-1 font-medium">
                   <p>02. 연구소 설립</p>
                   <p>03. 경상남도 스타트업 데모 IR 대회 수상</p>
                   <p>07. 한국전기연구원 기술이전 완료</p>
                 </div>
-                <div class="h-full absolute left-36 -top-7">
+                <div class="absolute h-full left-36 -top-7">
                   <div class="size-4 border-[3px] rounded-full border-gray-500 bg-white absolute top-9 z-10"></div>
                 </div>
             </li>
 
-            <li class="flex gap-32 relative">
-                <h1 class="text-blue-900 font-black text-3xl">2022'</h1>
-                <div class="flex flex-col gap-8 font-medium pt-1">
+            <li class="relative flex gap-32">
+                <h1 class="text-3xl font-black text-blue-900">2022'</h1>
+                <div class="flex flex-col gap-8 pt-1 font-medium">
                   <p>06. 제조빅데이터 포집 게이트 웨이 ‘R-GATE’ 런칭</p>
                   <p>09. AI 엣지 컴퓨팅 ‘R-BOX’ 런칭</p>
                 </div>
-                <div class="h-full absolute left-36 -top-7">
+                <div class="absolute h-full left-36 -top-7">
                   <div class="size-4 border-[3px] rounded-full border-gray-500 bg-white absolute top-9 z-10"></div>
                 </div>
             </li>
 
-            <li class="flex gap-32 relative">
-                <h1 class="text-blue-900 font-black text-3xl">2024'</h1>
-                <div class="flex flex-col gap-8 font-medium pt-1">
-                  <p>06. 경상남도 정보 보안 IR 대회ㅏ 수상</p>
+            <li class="relative flex gap-32">
+                <h1 class="text-3xl font-black text-blue-900">2024'</h1>
+                <div class="flex flex-col gap-8 pt-1 font-medium">
+                  <p>06. 경상남도 정보 보안 IR 대회 수상</p>
                 </div>
-                <div class="h-full absolute left-36 -top-7">
+                <div class="absolute h-full left-36 -top-7">
                   <div class="size-4 border-[3px] rounded-full border-gray-500 bg-white absolute top-9 z-10"></div>
                   <!-- <div class=" absolute h-full w-0.5 bg-gray-500 -z-10"></div> -->
                 </div>
